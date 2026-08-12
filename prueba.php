8a491f363605e68b04ec68ac63c9eb4db1d6f533
Agostina <adandini@frro.utn.edu.ar>
2026-08-12 11:20:56 -0300

    prueba de commit

--- prueba.php
+++ prueba.php
@@ -1,14 +1,10 @@
 <html>
     <head>
-<<<<<<< HEAD:prueba.html
         <title id="titulo">Hola mundo</title>
-=======
-        <title id="titulo">Te equivocaste santi</title>
->>>>>>> df199b549f3e26a7271955de1da01eb84a1758e9:prueba.php
     </head>
     <body>
         <h1>
-            <strong>HOLA</strong>
+            <strong>HOLitas</strong>
             <i>prueba 1</i>
         </h1>
     </body>
